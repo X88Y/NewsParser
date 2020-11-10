bddd2d3ce75bd5976528ba7f6ec9a7b221569460
X88Y <63887301+X88Y@users.noreply.github.com>
2020-11-10 12:53:08 +0300

    Add files via upload

---
+++ newsppr.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def news():
+    i = 0
+    result = []
+    url = 'https://ria.ru/lenta/'
+
+    source_code = requests.get(url)
+    html = bs(source_code.text, 'html.parser')
+    countdown = html.find_all('meta', {"itemprop": "name"})
+    
+    for new in countdown:
+        i = i + 1
+        if i != 2 and i != 3 and i != 4 and i < 10:
+            result.append(((str(new)[15:-19])))
+            result.append('')
+    return (str(result)[2:-2]).replace("\', \'", '\n')
